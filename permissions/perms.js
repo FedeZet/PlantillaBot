04c678badf6fee32c06f11cb6b8f0988b713139c
FedeZet <fcoelhovazquez@gmail.com>
2022-03-05 16:17:35 -0300

    Update perms.js

--- permissions/perms.js
+++ permissions/perms.js
@@ -3,7 +3,7 @@ const permAdmin = [
 		id: '', //ID del Comando
 		permissions: [
 			{
-				id: '200677250511667202', //ID del Rol o Usuario
+				id: '', //ID del Rol o Usuario
 				type: 2, // 1 = Rol ó 2 = Usuario
 				permission: true,
 			},
